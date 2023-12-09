c7a398b0373a8cad7b2afa70b03f7ba17b0dee30
Cristiano Sobral <cssobral2013@gmail.com>
2023-12-09 14:08:04 -0300

    Update v5.10

---
+++ generate-woff-woff2.py
@@ -0,0 +1,303 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/Fraternidad-Thin.ttf'
+woffPath = './web/Fraternidad-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Fraternidad-ThinItalic.ttf'
+woffPath = './web/Fraternidad-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Fraternidad-ExtraLight.ttf'
+woffPath = './web/Fraternidad-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Fraternidad-ExtraLightItalic.ttf'
+woffPath = './web/Fraternidad-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/Fraternidad-Light.ttf'
+woffPath = './web/Fraternidad-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-LightItalic.ttf'
+woffPath = './web/Fraternidad-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-LightItalic.ttf'
+woffPath = './web/Fraternidad-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-Regular.ttf'
+woffPath = './web/Fraternidad-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-Italic.ttf'
+woffPath = './web/Fraternidad-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-Medium.ttf'
+woffPath = './web/Fraternidad-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-MediumItalic.ttf'
+woffPath = './web/Fraternidad-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBold.ttf'
+woffPath = './web/Fraternidad-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBoldItalic.ttf'
+woffPath = './web/Fraternidad-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBold.ttf'
+woffPath = './web/Fraternidad-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBoldItalic.ttf'
+woffPath = './web/Fraternidad-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-Bold.ttf'
+woffPath = './web/Fraternidad-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-BoldItalic.ttf'
+woffPath = './web/Fraternidad-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-ExtraBold.ttf'
+woffPath = './web/Fraternidad-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Fraternidad-ExtraBoldItalic.ttf'
+woffPath = './web/Fraternidad-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+ 
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/Fraternidad-Thin.ttf'
+woff2Path = './web/Fraternidad-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Fraternidad-ThinItalic.ttf'
+woff2Path = './web/Fraternidad-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Fraternidad-ExtraLight.ttf'
+woff2Path = './web/Fraternidad-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Fraternidad-ExtraLightItalic.ttf'
+woff2Path = './web/Fraternidad-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/Fraternidad-Light.ttf'
+woff2Path = './web/Fraternidad-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-LightItalic.ttf'
+woff2Path = './web/Fraternidad-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-LightItalic.ttf'
+woff2Path = './web/Fraternidad-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-Regular.ttf'
+woff2Path = './web/Fraternidad-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-Italic.ttf'
+woff2Path = './web/Fraternidad-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-Medium.ttf'
+woff2Path = './web/Fraternidad-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-MediumItalic.ttf'
+woff2Path = './web/Fraternidad-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBold.ttf'
+woff2Path = './web/Fraternidad-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBoldItalic.ttf'
+woff2Path = './web/Fraternidad-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBold.ttf'
+woff2Path = './web/Fraternidad-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-SemiBoldItalic.ttf'
+woff2Path = './web/Fraternidad-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-Bold.ttf'
+woff2Path = './web/Fraternidad-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-BoldItalic.ttf'
+woff2Path = './web/Fraternidad-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-ExtraBold.ttf'
+woff2Path = './web/Fraternidad-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Fraternidad-ExtraBoldItalic.ttf'
+woff2Path = './web/Fraternidad-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
